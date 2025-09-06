986606c89568a29fe3b2e739e5dfdb148847ab7e
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2025-09-06 11:38:01 -0400

    Add example

--- src/schemas/mod.rs
+++ src/schemas/mod.rs
@@ -1,4 +1,15 @@
 pub mod custom;
+
+#[allow(
+    dead_code,
+    non_camel_case_types,
+    unused_mut,
+    unused_variables,
+    clippy::never_loop,
+    clippy::single_match,
+    clippy::redundant_field_names
+)]
+#[rustfmt::skip]
 pub mod generated;
 
 pub use custom::*;
